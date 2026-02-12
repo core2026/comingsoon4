c98c1a13b56881ee1370d219f89ac1153ab5ce6e
core2026 <acekallas@gmail.com>
2026-02-11 21:32:29 -0600

    Delete functions directory

--- functions/api.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  // This looks for the "API" binding you set in the Cloudflare dashboard
-  const { searchParams } = new URL(context.request.url);
-  const lat = searchParams.get('lat');
-  const lon = searchParams.get('lon');
-
-  // Forwards the request internally to your Worker
-  return context.env.API.fetch(`${context.request.url}`);
-}
